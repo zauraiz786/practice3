b0c64417f5863aa699ee9b45156f96372a6d5e97
zauraiz786 <zauraizhassankhan99@gmail.com>
2023-09-15 22:19:49 +0500

    first comment

---
+++ assignment 17-20/app8.js
@@ -0,0 +1,4 @@
+var a = [24,53,78,91,12];
+document.write(`Array items: ${a}<br>`);
+a.sort();
+document.write(`The largest number is ${a[a.length-1]}`);
---
+++ assignment 17-20/app9.js
@@ -0,0 +1,4 @@
+var a = [24,53,78,91,12];
+document.write(`Array items: ${a}<br>`);
+a.sort();
+document.write(`The smallest number is ${a[0]}`);
